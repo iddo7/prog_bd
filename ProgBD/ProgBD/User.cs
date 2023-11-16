1038f237ca471681270983968e4baf462f9b67a7
iddo <isaacnegreiros10@gmail.com>
2023-11-16 08:57:14 -0500

    Creating User Class

---
+++ ProgBD/ProgBD/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgBD
+{
+    internal class User
+    {
+        string username;
+        string password;
+
+        public User()
+        {
+            this.Username = string.Empty;
+            this.Password = string.Empty;
+        }
+
+        public User(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
