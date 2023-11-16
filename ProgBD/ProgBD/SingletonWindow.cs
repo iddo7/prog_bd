2740034f8d1c0475d913f2ac1dfd013393b098f3
iddo <isaacnegreiros10@gmail.com>
2023-11-16 09:09:38 -0500

    Creating Window Singleton

---
+++ ProgBD/ProgBD/SingletonWindow.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgBD
+{
+    internal class SingletonWindow
+    {
+        static SingletonWindow instance = null;
+        NavigationView navigationView;
+        MainWindow mainWindow;
+
+        public static SingletonWindow Instance()
+        {
+            if (instance == null) instance = new SingletonWindow();
+            return instance;
+        }
+
+        public NavigationView Nav
+        {
+            get { return navigationView; }
+            set { navigationView = value; }
+        }
+
+        public MainWindow MainWindow
+        {
+            get { return mainWindow; }
+            set { mainWindow = value; }
+        }
+    }
+}
