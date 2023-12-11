f5a0439e510bf3349743d990239a9159411dab5c
iddo <isaacnegreiros10@gmail.com>
2023-12-10 17:42:35 -0800

    Update Feedback.cs

--- ProgBD/ProgBD/Feedback.cs
+++ ProgBD/ProgBD/Feedback.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.UI.Xaml;
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +12,17 @@ namespace ProgBD
 {
     internal static class Feedback
     {
+
         /*   --- Dialog Box with only close button ---   */
         public static async Task<ContentDialogResult> VoidDialog(string title, string content, string closeButtonText)
         {
-            ContentDialog dialog = new ContentDialog();
-            dialog.XamlRoot = WindowSingleton.Instance().DialogPanel.XamlRoot;
-            dialog.Title = title;
-            dialog.Content = content;
-            dialog.CloseButtonText = closeButtonText;
+            ContentDialog dialog = new ContentDialog()
+            {
+                XamlRoot = WindowSingleton.Instance().DialogPanel.XamlRoot,
+                Title = title,
+                Content = content,
+                CloseButtonText = closeButtonText,
+            };
 
             return await dialog.ShowAsync();
         }
