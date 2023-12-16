a347354f4157d4ef9f22cea33dc8f9b45834f824
iddo <isaacnegreiros10@gmail.com>
2023-12-16 10:45:43 -0800

    Cleaning default comments

--- ProgBD/ProgBD/CreateProjectsPage.xaml.cs
+++ ProgBD/ProgBD/CreateProjectsPage.xaml.cs
@@ -14,14 +14,8 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using MySqlX.XDevAPI;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace ProgBD
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class CreateProjectsPage : Page
     {
         public CreateProjectsPage()
